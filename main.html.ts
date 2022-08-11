8765b23614c87761b74cf892fc92d359615f8011
deepakkumar640 <95094333+deepakkumar640@users.noreply.github.com>
2022-08-11 11:29:39 +0530

    Add files via upload

---
+++ main.html.ts
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Employee portal</title>
+</head>
+
+<body>
+
+    <div id="header">
+       
+      
+
+    <div id="content">
+
+     
+
+            </div>
+
+        
+
+               
+
+</body>
+</html>
+
+<button type="button">Click Me!</button>
